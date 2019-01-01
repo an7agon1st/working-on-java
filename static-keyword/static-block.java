3874e048420b65728cb343fe1e29d2f12debbd64
Tanzilur Rahman <rahman.tanzilur@outlook.com>
2019-01-02 00:27:23 +0530

    Checked syntax for static block added

---
+++ static-keyword/static-block.java
@@ -0,0 +1,9 @@
+class Main2 {
+    static {
+        System.out.println("This is executed outside and before..");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("...this which is in the main funtion");
+    }
+}
